bd14868c25f6fcad82b0e786a1ad1e6df97dc7a6
Black貓①呺 <jha334201553@163.com>
2020-07-01 18:41:12 +0800

    内存申请异常bug

--- Common/defs.h
+++ Common/defs.h
@@ -22,25 +22,58 @@
 #define INOUT
 #endif
 
+#ifdef _DEBUG
+#define CHECK_MEMORY   // 校验缓冲区溢出开关 
+#endif // _DEBUG
+
 #define HEAPMEMEOY      0x01
 #define MALLOCMEMEOY    0x02
+
+#define MEMOEY_FLAGS    0xA5
 
 #pragma pack(push,1)
 typedef struct _dynamic_memory 
 {
     unsigned long heap_size;
     unsigned int  heap_type;
-    unsigned char heap[1];
+    // 虽然下面heap定义在 heap_type 之后，但意思其实是在整个柔性数组的末尾字节 
+#ifdef CHECK_MEMORY
+    unsigned char heap[4];
+#else
+    unsigned char heap[2];
+#endif
 }dynamic_memory, *pdynamic_memory;
 #pragma pack(pop)
 
+#ifdef CHECK_MEMORY
+#include <crtdbg.h>
+// 检测内存是否溢出 
+static __forceinline VOID ASSERT_MEMORY_OVERFLOW(LPVOID _addr, LPCTSTR message) 
+{
+    if (_addr != NULL)
+    {
+        pdynamic_memory lpBuf = (pdynamic_memory)CONTAINING_RECORD(_addr, dynamic_memory,heap);
+        unsigned char* lpcheckbuf = lpBuf->heap + lpBuf->heap_size;
+        if (lpcheckbuf[0] != 0x00 || lpcheckbuf[1] != 0x00 ||
+            lpcheckbuf[2] != MEMOEY_FLAGS || lpcheckbuf[3] != MEMOEY_FLAGS)
+        {
+            // 发生了溢出 
+            _ASSERT_EXPR(NULL, message);
+        }
+    }
+}
+#else // CHECK_MEMORY
+#define ASSERT_MEMORY_OVERFLOW(...)
+#endif // CHECK_MEMORY
+#define ASSERT_MO(addr)  ASSERT_MEMORY_OVERFLOW(addr, TEXT("Memory is Overflow"))
 
-// #define AllocMemory(_nsize)  {int _a = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, _nsize); if (_a == NULL){printf("err: %d\n", GetLastError()); }}  // 按字节申请内存，在C运行库未初始化情况下就可用 
-static inline LPVOID AllocMemory(unsigned long _nsize) 
+// 申请内存,并全置零 
+static __forceinline LPVOID AllocMemory(unsigned long _nsize) 
 {
     pdynamic_memory lpBuf = (pdynamic_memory)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, _nsize+sizeof(dynamic_memory)); 
     if (lpBuf == NULL)
     {
+        // 内存申请失败，换个函数申请 
         lpBuf = (pdynamic_memory)malloc(_nsize+sizeof(dynamic_memory));
         if (lpBuf != NULL)
         {
@@ -57,47 +90,20 @@ static inline LPVOID AllocMemory(unsigned long _nsize)
         lpBuf->heap_type = HEAPMEMEOY; 
     }
     lpBuf->heap_size = _nsize;
+#ifdef CHECK_MEMORY
+    // 最后4个字节，前两个字节作为宽字符的结束符(以防一些函数不会加结束符),后两个字符用于校验溢出 
+    memset(lpBuf->heap+_nsize+sizeof(wchar_t), MEMOEY_FLAGS, 2);
+#endif
     return lpBuf->heap;
 }
 
-// #define ReAllocMemory(_addr, _nsize)  HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, _addr, _nsize) // 在原基础上增加、减少否内存 
-static inline LPVOID ReAllocMemory(LPVOID _addr, DWORD _nsize)
-{
-    pdynamic_memory lpBuf = (pdynamic_memory)CONTAINING_RECORD(_addr, dynamic_memory,heap);
-    if (lpBuf == NULL)
-    {
-        return AllocMemory(_nsize);
-    }
-    else
-    {
-        switch(lpBuf->heap_type)
-        {
-        case HEAPMEMEOY:
-            lpBuf = (pdynamic_memory)HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, lpBuf, _nsize+sizeof(dynamic_memory));
-            break;
-        case MALLOCMEMEOY:
-            lpBuf = (pdynamic_memory)realloc(lpBuf, _nsize+sizeof(dynamic_memory));
-            if (lpBuf != NULL && _nsize > lpBuf->heap_size)
-            {
-                memset(lpBuf->heap+lpBuf->heap_size, 0, _nsize-lpBuf->heap_size);
-            }
-            break;
-        }
-    }
-    if (lpBuf == NULL)
-    {
-        return NULL;
-    }
-
-    lpBuf->heap_size = _nsize;
-    return lpBuf->heap;
-}
-
-// #define FreeMemory(_addr)   { if (_addr) { HeapFree(GetProcessHeap(), HEAP_NO_SERIALIZE, _addr); _addr=NULL; } }  // 释放内存
-static inline VOID FreeMemory(LPVOID _addr)
+// 释放内存 
+#define FreeMemory(addr)   {FreeMemory_stub(addr); addr = NULL;} 
+static __forceinline VOID FreeMemory_stub(LPVOID _addr)
 {
     if (_addr != NULL)
     {
+        ASSERT_MO(_addr); // 如果开启检测溢出，释放内存自动检测 
         pdynamic_memory lpBuf = (pdynamic_memory)CONTAINING_RECORD(_addr, dynamic_memory,heap);
         if (lpBuf != NULL)
         {
@@ -113,6 +119,57 @@ static inline VOID FreeMemory(LPVOID _addr)
         }
         _addr = NULL;
     }
+}
+
+// 原基础上扩充或者缩减内存 （也可用于申请内存_addr为NULL) 
+static __forceinline LPVOID ReAllocMemory(LPVOID _addr, DWORD _nsize)
+{
+    if (_addr == NULL)
+    {
+        return AllocMemory(_nsize);
+    }
+
+    // 利用系统函数重新申请 
+    pdynamic_memory lpNewBuf = NULL;
+    pdynamic_memory lpBuf = (pdynamic_memory)CONTAINING_RECORD(_addr, dynamic_memory,heap);
+    switch(lpBuf->heap_type)
+    {
+    case HEAPMEMEOY:
+        lpNewBuf = (pdynamic_memory)HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, lpBuf, _nsize+sizeof(dynamic_memory));
+        break;
+    case MALLOCMEMEOY:
+        lpNewBuf = (pdynamic_memory)realloc(lpBuf, _nsize+sizeof(dynamic_memory));
+        if (lpNewBuf != NULL && _nsize > lpNewBuf->heap_size)
+        {
+            memset(lpNewBuf->heap+lpNewBuf->heap_size, 0, _nsize-lpNewBuf->heap_size);
+        }
+        break;
+    }
+
+    // 系统重新申请失败，试试自己申请拷贝 
+    if (lpNewBuf == NULL)
+    {
+        lpNewBuf = (pdynamic_memory)AllocMemory(_nsize+sizeof(dynamic_memory));
+        if (lpNewBuf)
+        {
+            // 把原来的数据拷贝过来，再释放内存 
+            memcpy(lpNewBuf->heap, _addr, lpBuf->heap_size>_nsize?_nsize:lpBuf->heap_size);
+            // 释放之前的buffer 
+            FreeMemory(_addr);
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    lpBuf->heap_size = _nsize;
+#ifdef CHECK_MEMORY
+    // 最后4个字节，前两个字节作为宽字符的结束符(以防一些函数不会加结束符),后两个字符用于校验溢出 
+//     memset(lpBuf->heap+_nsize, 0, sizeof(wchar_t));
+    memset(lpBuf->heap+_nsize+sizeof(wchar_t), 0, 2);
+#endif
+    return lpBuf->heap;
 }
 
 #define DeleteHandle(_handle) {if(_handle != INVALID_HANDLE_VALUE) { CloseHandle(_handle); _handle = INVALID_HANDLE_VALUE;}} // 关闭句柄 
